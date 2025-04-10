a2e10cbef34252277e179e10b53d5c5e2929c46f
lpxxn <mi_duo@live.com>
2025-04-10 10:21:14 +0800

    feat: update

---
+++ tests/main.go
@@ -0,0 +1,77 @@
+package main
+
+import (
+	"strings"
+	"time"
+
+	"github.com/lpxxn/snake/tests/game"
+	"github.com/nsf/termbox-go"
+)
+
+func main() {
+	// 初始化终端
+	err := termbox.Init()
+	if err != nil {
+		panic(err)
+	}
+	defer termbox.Close()
+
+	// 创建游戏实例
+	g := game.NewGame(60, 40)
+
+	// 创建事件通道
+	eventQueue := make(chan termbox.Event)
+	go func() {
+		for {
+			eventQueue <- termbox.PollEvent()
+		}
+	}()
+
+	// 游戏主循环
+	ticker := time.NewTicker(200 * time.Millisecond)
+	defer ticker.Stop()
+
+	for {
+		select {
+		case ev := <-eventQueue:
+			if ev.Type == termbox.EventKey {
+				switch ev.Key {
+				case termbox.KeyArrowUp:
+					g.ChangeDirection(game.Up)
+				case termbox.KeyArrowDown:
+					g.ChangeDirection(game.Down)
+				case termbox.KeyArrowLeft:
+					g.ChangeDirection(game.Left)
+				case termbox.KeyArrowRight:
+					g.ChangeDirection(game.Right)
+				case termbox.KeyEsc:
+					return
+				}
+			}
+		case <-ticker.C:
+			// 清屏
+			termbox.Clear(termbox.ColorDefault, termbox.ColorDefault)
+
+			// 移动蛇
+			g.Move()
+
+			// 绘制游戏状态
+			gameState := g.String()
+			lines := strings.Split(gameState, "\n")
+			for y, line := range lines {
+				for x, ch := range line {
+					termbox.SetCell(x, y, ch, termbox.ColorDefault, termbox.ColorDefault)
+				}
+			}
+
+			// 刷新屏幕
+			termbox.Flush()
+
+			// 检查游戏是否结束
+			if g.GameOver {
+				time.Sleep(2 * time.Second)
+				return
+			}
+		}
+	}
+}
